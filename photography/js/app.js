97631c4969cf8f4d1657fa761380774fe8a01a6e
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2022-05-29 13:46:35 +0530

    .

--- photography/js/app.js
+++ photography/js/app.js
@@ -9,7 +9,7 @@ for (let i = 1; i <= 4; i++) {
     image.classList.add('galleryImg');
     image.addEventListener('click', () => {
         popup.style.transform = 'translateY(0)';
-        selectedImage.src = 'https://ik.imagekit.io/nishithpshetty/photography/tr:w-400,h-350/' + i + '.jpg';
+        selectedImage.src = 'https://ik.imagekit.io/nishithpshetty/photography/' + i + '.jpg';
     });
     gallery.appendChild(image);
 };
